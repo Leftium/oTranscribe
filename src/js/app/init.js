1dfb501f04f81ee5d1d9e60ae283868b0bf804ce
Leftium <john@leftium.com>
2020-04-23 17:18:57 -0500

    Support TAB key in editor to indent.

--- src/js/app/init.js
+++ src/js/app/init.js
@@ -229,5 +229,26 @@ function handleDrop (e) {
 document.body.addEventListener('dragover', handleDragover);
 document.body.addEventListener('drop', handleDrop);
 
+let textbox = document.getElementById("textbox");
+textbox.addEventListener('keydown', function (e) {
+    if (e.keyCode === 9) { // tab key
+        e.preventDefault();  // this will prevent us from tabbing out of the editor
+
+        // now insert four non-breaking spaces for the tab key
+        var editor = textbox
+        var doc = editor.ownerDocument.defaultView;
+        var sel = doc.getSelection();
+        var range = sel.getRangeAt(0);
+
+        var tabNode = document.createTextNode("\u00a0\u00a0\u00a0\u00a0");
+        range.insertNode(tabNode);
+
+        range.setStartAfter(tabNode);
+        range.setEndAfter(tabNode); 
+        sel.removeAllRanges();
+        sel.addRange(range);
+    }
+});
+
 
 
